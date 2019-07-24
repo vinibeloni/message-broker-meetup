40ccd752c7a64f17c71f6fe832aa07f09b58bd59
Vinicius Beloni <viniciusbcubas@outlook.com>
2019-07-24 16:13:49 -0300

    initial files

---
+++ src/simple-queue/Consumer/Program.fs
@@ -0,0 +1,27 @@
+﻿open RabbitMQ.Client.Events
+open RabbitMQ.Client
+open System.Text
+open System
+
+[<EntryPoint>]
+let main argv =
+    let factory = ConnectionFactory(HostName = "localhost")
+    use connection = factory.CreateConnection()
+    use channel = connection.CreateModel()
+    
+    channel.QueueDeclare("hello", false, false, false, null) |> ignore
+    
+    let consumer = EventingBasicConsumer(channel)
+    
+    consumer.Received.Add(fun message ->
+        message.Body
+        |> Encoding.UTF8.GetString 
+        |> printfn " Receive a message: %s"
+    )
+    
+    channel.BasicConsume("hello", true, consumer) |> ignore
+
+    printfn " Press [enter] to exit."
+    Console.ReadLine() |> ignore
+
+    0 // exit code
---
+++ src/simple-queue/Publisher/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.Text
+open System
+open RabbitMQ.Client
+
+[<EntryPoint>]
+let main argv =
+
+    let factory = ConnectionFactory(HostName = "localhost")
+    let connection = factory.CreateConnection()
+    let channel = connection.CreateModel()
+
+    channel.QueueDeclare("hello",false,false,false, null) |> ignore
+
+    let message = argv |> String.concat ","
+    let body = Encoding.UTF8.GetBytes message
+
+    let properties = channel.CreateBasicProperties();
+    channel.BasicPublish("", "hello", properties, body)
+    
+    printfn " Press [enter] to exit."
+    Console.ReadLine() |> ignore
+
+    0 // exit code
